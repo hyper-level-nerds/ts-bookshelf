58ecad5e5e7d75944ee6b9449dfe8e14496b48ad
Sophia <me@sophia-dev.io>
2022-12-08 01:48:45 +0900

    chore: initial commit 📚 [skip ci]

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">
+  <br />
+  📚
+  <br />
+  tmd
+  <sup>
+    <br />
+    <br />
+  </sup>    
+</h1>
+
+<div align="center">
+    <br />
+    <sup>generate markdown-based documentation from typescript types</sup>
+    <br />
+    <br />
+</div>
+
+## Introduction
+
+`tmd (tee-em-dee)` is a tool for generating markdown documentation from typescript types. 
---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+const jestConfig: JestConfigWithTsJest = {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: "./src",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest",
+    },
+    collectCoverageFrom: ["**/*.ts", "!coverage/**", "!utils/noop.ts", "!index.ts", "!app.ts"],
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+    roots: ["<rootDir>"],
+    modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/../" }),
+    collectCoverage: false,
+    coverageThreshold: {
+        global: {
+            lines: 0,
+            branches: 0,
+            functions: 0,
+            statements: 0,
+        },
+    },
+};
+
+export = jestConfig;
