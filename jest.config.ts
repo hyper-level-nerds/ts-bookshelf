81f75c772e6b94dc21b2feaedffec471394c0d24
Sophia <me@sophia-dev.io>
2022-12-09 05:03:36 +0900

    test: make test suite not to collect from `index.ts` files

--- jest.config.ts
+++ jest.config.ts
@@ -9,12 +9,12 @@ const jestConfig: JestConfigWithTsJest = {
     transform: {
         "^.+\\.(t|j)s$": "ts-jest",
     },
-    collectCoverageFrom: ["**/*.ts", "!coverage/**", "!utils/noop.ts", "!index.ts", "!app.ts"],
+    collectCoverageFrom: ["**/*.ts", "!coverage/**", "!utils/noop.ts", "!index.ts", "!app.ts", "!**/index.ts"],
     coverageDirectory: "../coverage",
     testEnvironment: "node",
     roots: ["<rootDir>"],
     modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/../" }),
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
     collectCoverage: false,
     coverageThreshold: {
         global: {
